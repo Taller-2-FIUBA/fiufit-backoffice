17b4ac5acc84cbea2098de653bb5802189ec471d
Sol Orive <sorive@MacBook-Pro-de-Sol.local>
2023-04-13 19:40:05 -0300

    Agrego la edición de una row para agregar un campo a la tabla de admins

--- src/components/admins/Admins.tsx
+++ src/components/admins/Admins.tsx
@@ -11,11 +11,22 @@ import {
   Paper,
   Button,
 } from "@mui/material";
-import { Admin, getAdmins } from "../../api/AdminsService";
+
+import { Admin, getAdmins, createAdmin } from "../../api/AdminsService";
 
 const AdminsTablePage: React.FC = () => {
   const [admins, setAdmins] = useState<Admin[]>([]);
   const [error, setError] = useState<string>("");
+
+  const [newAdmin, setNewAdmin] = useState<Admin>({
+    id: "",
+    email: "",
+    password: "",
+    username: "",
+  });
+  const [isAdding, setIsAdding] = useState(false);
+  const [addAdminButtonText, setAddAdminButtonText] =
+    useState<string>("+ Agregar Admin");
 
   useEffect(() => {
     const fetchAdmins = async () => {
@@ -31,8 +42,32 @@ const AdminsTablePage: React.FC = () => {
   }, []);
 
   // Función para agregar un nuevo admin a la lista
-  const handleAddAdmin = () => {
-    console.log("agregar admin");
+  const handleAddAdmin = async () => {
+    if (isAdding) {
+      console.log("Guardar admin");
+
+      try {
+        const savedAdmin = await createAdmin(newAdmin);
+        setAdmins([...admins, savedAdmin]); // Agrego el nuevo admin a la lista
+
+        // OK
+      } catch (error) {
+        //TODO esto para el caso de 500, sino identificar el error en particular
+        setError("Error al guardar el admin. Por favor, inténtelo de nuevo.");
+      }
+
+      setIsAdding(false); // Vuelvo a la vista normal
+      setAddAdminButtonText("+ Agregar Admin");
+    } else {
+      setIsAdding(true);
+      setNewAdmin({ id: "", username: "", email: "", password: "" }); // Limpio el campo
+      setAddAdminButtonText("Guardar"); // Cambiar el texto del botón a "Guardar"
+    }
+  };
+
+  const handleNewAdminChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setNewAdmin({ ...newAdmin, [name]: value });
   };
 
   return (
@@ -44,6 +79,7 @@ const AdminsTablePage: React.FC = () => {
               <TableCell>ID</TableCell>
               <TableCell>Username</TableCell>
               <TableCell>Email</TableCell>
+              <TableCell>Password</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -52,8 +88,47 @@ const AdminsTablePage: React.FC = () => {
                 <TableCell>{admin.id}</TableCell>
                 <TableCell>{admin.username}</TableCell>
                 <TableCell>{admin.email}</TableCell>
+                <TableCell> *** </TableCell>
               </TableRow>
             ))}
+            {isAdding && (
+              <TableRow>
+                <TableCell>
+                  <input
+                    type="text"
+                    name="id"
+                    value={newAdmin.id}
+                    disabled
+                    readOnly={true}
+                    style={{ backgroundColor: "#f2f2f2" }}
+                  />
+                </TableCell>
+                <TableCell>
+                  <input
+                    type="text"
+                    name="username"
+                    value={newAdmin.username}
+                    onChange={handleNewAdminChange}
+                  />
+                </TableCell>
+                <TableCell>
+                  <input
+                    type="text"
+                    name="email"
+                    value={newAdmin.email}
+                    onChange={handleNewAdminChange}
+                  />
+                </TableCell>
+                <TableCell>
+                  <input
+                    type="text"
+                    name="password"
+                    value={newAdmin.password}
+                    onChange={handleNewAdminChange}
+                  />
+                </TableCell>
+              </TableRow>
+            )}
           </TableBody>
         </Table>
         {error && (
@@ -68,7 +143,7 @@ const AdminsTablePage: React.FC = () => {
         onClick={handleAddAdmin}
         style={{ marginTop: 20, backgroundColor: "#45C4B0" }}
       >
-        + Agregar Admin
+        {addAdminButtonText} {/* Me devuelve el texto del botón */}
       </Button>
     </Container>
   );
