69fdd0a5c6f01ada59ba4e0600842bda64c47070
Belovic94 <victor.belosevich94@gmail.com>
2023-04-25 03:25:16 -0300

    Improve profile

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,37 +1,26 @@
-import { Avatar, Card, CardContent, Box, Stack, Paper, styled } from '@mui/material';
+import { Avatar, Card, CardContent, Box, Stack} from '@mui/material';
 import React from 'react';
 import './Profile.scss';
 import { User, getUser} from '../../api/UsersService';
 import { useParams } from 'react-router-dom';
+import * as MUIcon from "@mui/icons-material";
 
 interface ItemProps {
   title: string;
   value?: string;
+  icon?: keyof typeof MUIcon
 }
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
-
-const ProfileItem: React.FC<ItemProps> = ({title, value}) => {
+const ProfileItem: React.FC<ItemProps> = ({title, value, icon}) => {
+  const Icon = icon && MUIcon[icon]
   return (
-    <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 2 }}>
-      <Item className="user-profile-item"
-        sx={{
-          my: 1,
-          mx: 'auto',
-          p: 1,
-        }}
-      >
-        <Stack spacing={2} direction="row" alignItems="center" justifyContent="space-between">
-          <h3>{title}:</h3>
+        <Stack className='profile-item' spacing={2} direction="row" alignItems="center" justifyContent="space-between" >
+          <div className='icon-title-container'>
+            {Icon && <Icon fontSize="medium" sx={{ px: 1 }}/>}
+            <h3 className='profile-item-title'>{title}</h3>
+          </div>
           <p>{value}</p>
         </Stack>
-      </Item>
-    </Box> 
   )
 }
 
@@ -54,18 +43,26 @@ const Profile = () => {
   return (
     <Card className="user-profile">
       <CardContent>
-        {user && <Avatar className='user-avatar'
-          alt={user?.username}
-          src={user?.avatar}
-          >{user?.name[0] + user?.surname[0]}</Avatar>
+        {user && 
+          <div>
+            <Avatar className='user-avatar'
+              alt={user?.username}
+              src={user?.avatar}
+            ></Avatar>
+            <p className='user-name'>{user?.username}</p>
+          </div>
         }
-        <div className="user-details">
-          <ProfileItem title='First Name' value={user?.username}/>
-          <ProfileItem title="Last Name" value={user?.surname}/>
-          <ProfileItem title="Email" value={user?.email}/>
-          <ProfileItem title="Registration Date" value={user?.registration_date}/>
-          <ProfileItem title="role" value={user?.is_athlete ? "Athlete" : "Trainer"}/>
-        </div>
+        <Box className="user-details" sx={{ flexGrow: 1, overflow: 'hidden'}}>
+          <ProfileItem title='Name' value={user?.name} icon="Person"/>
+          <ProfileItem title="Surname" value={user?.surname} icon="Person"/>
+          <ProfileItem title="Email" value={user?.email} icon="Email"/>
+          <ProfileItem title="Registration Date" value={user?.registration_date} icon="CalendarToday"/>
+          <ProfileItem title="Birth Date" value={user?.birth_date} icon="Cake"/>
+          <ProfileItem title="Location" value={user?.location} icon="LocationOn"/>
+          <ProfileItem title="Weight" value={user?.weight?.toString()} />
+          <ProfileItem title="Height" value={user?.height?.toString()} />
+          <ProfileItem title="Role" value={user?.is_athlete ? "Athlete" : "Trainer"} />
+        </Box>
       </CardContent>
     </Card>
   );
