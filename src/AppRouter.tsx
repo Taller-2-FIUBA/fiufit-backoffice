da4094a14b68bbe092618c064ea68bac5a659074
Belovic94 <victor.belosevich94@gmail.com>
2023-04-25 22:29:10 -0300

    Change login location

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Navigate, Route, Routes } from 'react-router';
 import Profile from './components/profile/Profile';
 import Users from './components/users/Users';
-import LoginScreen from "./components/admins/Login";
+import LoginScreen from "./components/login/Login";
 import Admins from "./components/admins/Admins";
 import Trainings from './components/trainings/Trainings';
 
