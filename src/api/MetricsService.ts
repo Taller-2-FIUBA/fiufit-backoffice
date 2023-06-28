4ae523ff6a4c194326dd467a22e3e19d7deded3a
Sol Orive <sorive@MacBook-Pro-WTW.local>
2023-06-27 21:19:48 -0300

    Pegada al back para traer métricas

--- src/api/MetricsService.ts
+++ src/api/MetricsService.ts
@@ -1,16 +1,53 @@
+import { doFetch } from "./utils/fetchUtils";
+
 export interface UsersMetric {
-    id: string;
-    name: string,
-    value: number,
+    label: string,
+    count: number,
 }
 
+export interface Metrics {
+    name: string,
+    value: number
+}
+
+const baseMetricsUrl = `${process.env.REACT_APP_API_URL}`;
+
+const metricsToShow = [
+    'user_blocked_count'
+];
+
+async function getMetric(metricName: string): Promise<UsersMetric[]> {
+    return doFetch(baseMetricsUrl + `/metrics?name=${metricName}`, false, {
+        method: 'GET'
+    });
+}
+
+export async function getAllMetrics(){
+    const miArr: Metrics[] = []
+
+    try {
+        for (const e of metricsToShow) {
+        console.log("Nombre", e)
+         getMetric(e)
+         .then((responseBlockedUsers: UsersMetric[]) => {
+                const primerElemento = responseBlockedUsers[0];
+                miArr.push({name: e, value: primerElemento.count})
+                console.log(primerElemento);
+              })
+        }
+    }catch{
+        console.log(Error)
+    }
+    return miArr
+}
+/*
 export async function getUsersMetrics(): Promise<UsersMetric[]> {
     //try {
         //const response = await fetch(`${process.env.REACT_APP_USERS_METRICS_URL}`);
         //if (response.ok) {
         //    const usersMetricsResponse = await response.json();
             const metrics = [
-                { id: '01', name: 'Usuarios utilizando mail y contraseña', value: 100 },
+                { id: '01', name: 'Blocked users', value: 100 },
                 { id: '02', name: 'Usuarios utilizando identidad federada', value: 50 },
                 { id: '03', name: 'Login con mail y contraseña', value: 200 },
                 { id: '04', name: 'Login con identidad federada', value: 75 },
@@ -25,4 +62,4 @@ export async function getUsersMetrics(): Promise<UsersMetric[]> {
     } catch (error: any) {
         throw new Error(`Failed to fetch users metrics: ${error.message}`);
     } */
-}
+/*}*/
--- src/components/metrics/Metrics.tsx
+++ src/components/metrics/Metrics.tsx
@@ -1,39 +1,30 @@
-import React from 'react';
-import './Metrics.scss';
-import { Grid, Typography, Card } from '@mui/material';
-import { getUsersMetrics, UsersMetric } from '../../api/MetricsService';
+import React, { useEffect } from "react";
+import "./Metrics.scss";
+import { Grid, Typography, Card } from "@mui/material";
+import { getAllMetrics, Metrics } from "../../api/MetricsService";
 
+export default function Metric() {
+  const [usersMetrics, setUsersMetrics] = React.useState<Metrics[] | null>();
 
-const Metrics = () => {
-
-  const [usersMetrics, setUsersMetrics] = React.useState<UsersMetric[] | undefined>();
-
-  React.useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
-      const data: UsersMetric[] | undefined = await getUsersMetrics();
+      const data: Metrics[] = await getAllMetrics();
       setUsersMetrics(data);
-    }
-
-    fetchData()
-      .catch(console.error);
+    };
   }, []);
 
   return (
     <Grid container spacing={3} className="dashboard">
-        {usersMetrics?.map((metric) => (
-          <Grid item xs={12} sm={6} key={metric.name}>
-            <Card className="card">
-              <Typography variant="h5" component="h2" className="cardTitle">
-                {metric.name}
-              </Typography>
-              <Typography variant="body1">
-                {metric.value}
-              </Typography>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+      {usersMetrics?.map((metric) => (
+        <Grid item xs={12} sm={6} key={metric.name}>
+          <Card className="card">
+            <Typography variant="h5" component="h2" className="cardTitle">
+              {metric.name}
+            </Typography>
+            <Typography variant="body1">{metric.value}</Typography>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   );
-};
-
-export default Metrics;
+}
