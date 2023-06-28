01af7a58271132f1fe23ad09fbf4c8bc9344403f
Sol Orive <sorive@MacBook-Pro-WTW.local>
2023-06-28 10:15:14 -0300

    Dejo el array listo para iterar sobre todas las métricas

--- src/api/MetricsService.ts
+++ src/api/MetricsService.ts
@@ -27,34 +27,11 @@ export async function getAllMetrics(): Promise<MetricsResponse[]>{
     const miArr: MetricsResponse[] = []
     
     const m = await getMetric('user_blocked_count')
-
+    for (const metric of metricsToShow) {
         const primerElemento = m[0];
-        miArr.push({title: "Users blocked", name: 'user_blocked_count', value: primerElemento.count})
+        miArr.push({title: "Users blocked", name: metric, value: primerElemento.count})
         console.log(primerElemento);
-      
+    }
     console.log("RESULTADO DEL SERVICE", miArr)
     return miArr
 }
-/*
-export async function getUsersMetrics(): Promise<UsersMetric[]> {
-    //try {
-        //const response = await fetch(`${process.env.REACT_APP_USERS_METRICS_URL}`);
-        //if (response.ok) {
-        //    const usersMetricsResponse = await response.json();
-            const metrics = [
-                { id: '01', name: 'Blocked users', value: 100 },
-                { id: '02', name: 'Usuarios utilizando identidad federada', value: 50 },
-                { id: '03', name: 'Login con mail y contraseña', value: 200 },
-                { id: '04', name: 'Login con identidad federada', value: 75 },
-                { id: '05', name: 'Usuarios bloqueados', value: 10 },
-                { id: '06', name: 'Recupero de contraseña', value: 20 },
-                { id: '07', name: 'Usuarios por zona geográfica', value: 150 },
-              ];
-              return metrics;
-     /*    } else {
-            throw new Error(`Request failed with status ${response.status}`);
-        }
-    } catch (error: any) {
-        throw new Error(`Failed to fetch users metrics: ${error.message}`);
-    } */
-/*}*/
