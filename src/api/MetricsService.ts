796158aa58b1d018e899bcd98a8d8eae2467727d
Sol Orive <sorive@MacBook-Pro-WTW.local>
2023-06-28 21:43:03 -0300

    metricas desde el back

--- src/api/MetricsService.ts
+++ src/api/MetricsService.ts
@@ -7,15 +7,17 @@ export interface UsersMetric {
 
 export interface MetricsResponse {
     title: string,
-    name: string,
-    value: number
+    value: UsersMetric[]
 }
 
 const baseMetricsUrl = `${process.env.REACT_APP_API_URL}`;
 
 const metricsToShow = [
-    {title: "Users blocked", name: 'user_blocked_count'}
-    //TODO agregar acá el resto de métricas que vamos a mostrar
+    {title: "Users blocked", name: 'user_blocked_count', options: []},
+    {title: "Users Recovery password", name: 'user_password_recovery_count', options: []},
+    {title: "Login count", name: 'user_login_count', options: ['using_email_password', 'using_idp']},
+    {title: "Created count ", name: 'user_created_count', options: ['using_idp','using_email_password']},
+    {title: "User by location", name: 'user_by_region_count', options: []}
 ];
 
 async function getMetric(metricName: string): Promise<UsersMetric[]> {
@@ -25,13 +27,36 @@ async function getMetric(metricName: string): Promise<UsersMetric[]> {
 }
 
 export async function getAllMetrics(): Promise<MetricsResponse[]>{
-    const miArr: MetricsResponse[] = []
+
+console.log("llamada uno")
+    var miArr: MetricsResponse[] = []
     
-    for (const metric of metricsToShow) {
-        const m = await getMetric(metric.name)
-        const primerElemento = m[0];
-        miArr.push({title: metric.title, name: metric.name, value: primerElemento.count})
-        console.log(primerElemento);
+    for (var metric of metricsToShow) {
+        console.log("metric name", metric.name)
+        
+        const response: UsersMetric[] = await getMetric(metric.name)
+        var lista:UsersMetric[] = []
+
+        for(var i=0 ; i <= response.length; i++){
+            console.log("elemento", response)
+
+            const element = response[i];
+
+            if(element){ //Esto pasa porque a veces viene undefines
+                console.log("Estoy guardando", element);               
+                lista.push({
+                    label: element.label,
+                    count:  element? element.count : 0
+
+                })
+            }
+            
+        }
+        miArr.push({
+            title: metric.title, 
+            //name: metric.name, 
+            value: lista
+        })
     }
     console.log("RESULTADO DEL SERVICE", miArr)
     return miArr
--- src/components/metrics/Metrics.tsx
+++ src/components/metrics/Metrics.tsx
@@ -1,18 +1,29 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./Metrics.scss";
 import { Grid, Typography, Card } from "@mui/material";
 import { getAllMetrics, MetricsResponse } from "../../api/MetricsService";
 
-export default function Metrics() {
-  const [usersMetrics, setUsersMetrics] = React.useState<MetricsResponse[]>();
+function useMetrics() {
+  const [usersMetrics, setUsersMetrics] = useState<MetricsResponse[]>();
 
   useEffect(() => {
+    console.log(usersMetrics);
+
     const getMetrics = async () => {
       const lametrica = await getAllMetrics();
       setUsersMetrics(lametrica);
     };
-    getMetrics();
-  }, []);
+
+    if (!usersMetrics) {
+      getMetrics();
+    }
+  }, [usersMetrics]);
+
+  return usersMetrics;
+}
+
+export default function Metrics() {
+  const usersMetrics = useMetrics();
 
   const divStyle = {
     color: "white",
@@ -21,15 +32,50 @@ export default function Metrics() {
   return (
     <Grid container spacing={3} className="dashboard">
       {usersMetrics?.map((metric) => (
-        <Grid item xs={12} sm={6} key={metric.name}>
-          <Card className="card">
-            <Typography variant="h5" component="h2" className="cardTitle">
-              {metric.title}
-            </Typography>
-            <Typography variant="body1" style={{ marginTop: "10px" }}>
-              {metric.value}
-            </Typography>
-          </Card>
+        <Grid item xs={12} sm={6} key={metric.title}>
+          {metric.title == "User by location" ? (
+            <Card className="card-extended">
+              <Typography variant="h5" component="h2" className="cardTitle">
+                {metric.title}
+              </Typography>
+              <Grid container spacing={1} style={{ marginTop: "10px" }}>
+                {metric.value.length > 0 ? (
+                  metric.value.map((v, i) => (
+                    <Grid item xs={12} key={i}>
+                      <Typography>{v.label}</Typography>
+                      <Typography>{v.count ? v.count : 0}</Typography>
+                    </Grid>
+                  ))
+                ) : (
+                  <Grid item xs={12}>
+                    <Typography>total</Typography>
+                    <Typography>{0}</Typography>
+                  </Grid>
+                )}
+              </Grid>
+            </Card>
+          ) : (
+            <Card className="card">
+              <Typography variant="h5" component="h2" className="cardTitle">
+                {metric.title}
+              </Typography>
+              <Grid container spacing={1} style={{ marginTop: "10px" }}>
+                {metric.value.length > 0 ? (
+                  metric.value.map((v, i) => (
+                    <Grid item xs={12} key={i}>
+                      <Typography>{v.label}</Typography>
+                      <Typography>{v.count ? v.count : 0}</Typography>
+                    </Grid>
+                  ))
+                ) : (
+                  <Grid item xs={12}>
+                    <Typography>total</Typography>
+                    <Typography>{0}</Typography>
+                  </Grid>
+                )}
+              </Grid>
+            </Card>
+          )}
         </Grid>
       ))}
     </Grid>
